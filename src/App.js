c6febf6337b94a94fc1dfdabdbf8985c183122b6
Hayden West <haydenw7@gmail.com>
2021-09-08 11:42:12 -0700

    deck is created on page load

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./styles/App.css";
+import Deck from "./hooks/deck.js";
 
 function App() {
+  const deck = new Deck();
+  console.log(deck.cards);
+
   return (
     <div className="App">
       <h1>Hello There</h1>
